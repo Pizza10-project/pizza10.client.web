939cbb91bbd872d350dd09cbf27915a4213f82c9
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-10 13:12:42 -0300

    feat(home): add responsivity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,46 +6,46 @@ import { NavBar } from '../components/Nav'
 
 const Home: NextPage = () => {
   return (
-    <Container>
+    <Container p="20px">
       <Header description='Peça rápido, aprecie lentamente.' title="Pizza 10 - Rápida e deliciosa" />
       <NavBar />
-      <Flex backgroundColor={"primary.950"}
+      <Flex backgroundColor="primary.950"
         alignItems='center'
-        justifyContent="center"
+        border="2px solid"
+        borderColor="primary.800"
+        boxShadow='2px -2px 24px 0px rgba(214,52,71,0.75);'
+        h={["500px", "460px"]}
         gap="80px"
-        h="500px"
         borderRadius="24px">
-        <Box w="40%"
-        >
-          <Image src="/entregador.png" alt="" />
+        <Box w="40%" display={["none", "flex"]}>
+          <Image src="/entregador.png" backgroundSize='cover' alt="" />
         </Box>
-        <Box w="60%" letterSpacing={'wide'} p="30px">
+        <Box w={["100%", "60%"]} letterSpacing={['widest', "wide"]} p={["24px", "32px"]}>
           <Flex
             backgroundColor="primary.100"
             justifyContent="center"
             color="primary.900"
-            w="100%"
+            display={["none", "flex"]}
             p="12px"
             borderRadius="12px"
-          > ELEITO O MELHOR FASTFOOD DELIVERY  DE SÃO PAULO! </Flex>
-          <Box>
-            <Heading fontSize="6xl" color="white">Peça rápido, aprecie </Heading>
-            <Heading fontSize="6xl" color="primary.500"> lentamente.</Heading>
+          > ELEITO O MELHOR DELIVERY  DE SÃO PAULO! </Flex>
+          <Box my="24px">
+            <Heading fontSize="5xl" color="white">Peça rápido, aprecie </Heading>
+            <Heading fontSize="5xl" color="primary.500"> lentamente.</Heading>
           </Box>
           <Text
             fontSize="18px"
             my="18px"
             color="white"
-          >Pizzas com massa feita na hora, <br /> assada na pedra no forno a lenha!</Text>
+          >Pizzas com massa feita na hora, assada na pedra no forno a lenha!</Text>
 
           <Button
-            variant="outline"
+            variant="solid"
             size="lg"
             borderRadius="24px"
-            px="40px"
-            border="1px solid"
-            borderColor="primary.500"
-            color="primary.800"
+            my="12px"
+            px={["95px", "40px"]}
+            color="primary.900"
             _hover={{ backgroundColor: "primary.600", color: "primary.900" }}
             backgroundColor="primary.500"
 
