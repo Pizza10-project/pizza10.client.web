3c180ca9275c694fa36add353bf472fa19e53e8c
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-27 14:30:55 -0300

    fix: stripe images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    domains: [
+      'files.stripe.com',
+      "s3-alpha-sig.figma.com"
+    ],
+  },
+
+  experimental: {
+    newNextLinkBehavior: true,
+    images: {
+      allowFutureImage: true,
+    },
+  },
 }
 
 module.exports = nextConfig
